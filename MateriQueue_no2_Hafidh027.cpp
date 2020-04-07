5f3ddd9ed560d32cbd0cd51f72bfb91adcf976d3
HAFIDH AHMAD FAUZAN 2019 MI A <56226220+hafidh2001@users.noreply.github.com>
2020-04-07 19:11:11 +0700

    Add files via upload

---
+++ MateriQueue_no2_Hafidh027.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>  // perintah "printf" "scanf"
+#include <conio.h>  // perintah "getch"
+#include <stdlib.h>
+#define MAX 8     // Mendeklarasikan const
+
+// Membuat struct
+typedef struct
+{
+	int data[MAX];
+	int head;
+	int tail;
+} Queue;
+
+Queue antrian;
+
+void Create()
+{
+	antrian.head = antrian.tail = -1;
+}
+
+int IsEmpty()
+{
+	if( antrian.tail == -1 )
+		return 1;
+	else
+		return 0;
+}
+
+int IsFull()
+{
+	if( antrian.tail == MAX-1 )
+		return 1;
+	else
+		return 0;
+}
+
+void Enqueue(int data)
+{
+	if( IsEmpty() == 1 )
+	{
+		antrian.head = antrian.tail = 0;
+		antrian.data[antrian.tail] = data;
+		printf("%d masuk!",antrian.data[antrian.tail]);
+	} else;
+	
+	if( IsFull() == 0 )
+	{
+		antrian.tail++;
+		antrian.data[antrian.tail] = data;
+		printf("%d masuk!", antrian.data[antrian.tail]);
+	}
+}
+
+int Dequeue()
+{
+	int i;
+	int e = antrian.data[antrian.head];
+	for( i = antrian.head;i <= antrian.tail-1; i++ )
+	{
+		antrian.data[i] = antrian.data[i+1];
+	}
+	antrian.tail--;
+	return e;
+}
+
+void Tampil()
+{
+	int jum = 0;
+	if( IsEmpty() == 0 )
+	{
+		for( int i = antrian.head; i <= antrian.tail; i++ )
+		{
+			printf("%d", antrian.data[i]);
+			jum = jum + antrian.data[i];
+		}
+	}
+		else printf("data kosong!\n");
+}
+
+int main()
+{
+	// Mendeklarasikan variabel lokal
+	int pil;
+	int data;
+	
+	// Membaca function void
+	Create();
+	
+	// Menggunakan perulangan do while
+	do{
+		system ("CLS");
+		
+		// Menampilkan keterangan
+		printf("ANTRIAN PARKIRAN MOBIL\n");
+		printf("====================================================\n");
+		printf("1. Antrian Parkir Mobil\n");
+		printf("2. Keluarkan Mobil\n");
+		printf("3. Tampilkan Antrian Mobil\n");
+		printf("4. Exit\n");
+		printf("\n");
+		printf("Pilihan Anda = ");
+		scanf("%d",&pil);
+		printf("n");
+		
+		// Percabangan
+		switch(pil)
+		{
+			case 1: printf("Masukkan plat nomor mobil: ");scanf("%d", &data); Enqueue(data); break;
+			case 2: printf("Plat nomor mobil yang Keluar : %d", Dequeue() ); break;
+			case 3: Tampil(); break;
+	
+		getch();
+		} 
+	// Perulangan do while
+	} while(pil!=5);
+}
